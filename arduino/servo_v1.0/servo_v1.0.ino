0bbc5fcc5411080ca8fa0baa5fd42d594619953b
Adam <34109635+aaccttrr@users.noreply.github.com>
2018-04-16 22:12:01 +0100

    Added arduino code + HX711 libraries and examples

---
+++ arduino/servo_v1.0/servo_v1.0.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+const int pinServo = 3;
+
+Servo groveServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  groveServo.attach(pinServo);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  groveServo.write(90);
+  delay(1000);
+  groveServo.write(-90);
+  delay(1000);
+}
