a7b430de8be2f52b81b09f36d80b9affe56b938e
Adam <34109635+aaccttrr@users.noreply.github.com>
2018-04-17 22:19:25 +0100

    Added new version of arduino code
    Arduino code that uses random numbers in place of load cell input

---
+++ arduino/servo_v2.0/servo_v2.0.ino
@@ -0,0 +1,33 @@
+//Adam Cotter
+//Working using random numbers instead of laod cell input
+
+#include <Servo.h>
+#include "HX711.h"
+
+const int pinServo = 3;
+long weight;
+
+Servo groveServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  groveServo.attach(pinServo);
+  randomSeed(42);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //generate random number
+  weight = random(0,5);
+  delay(1000);
+
+  if(weight == 0){
+    groveServo.write(90);
+    delay(1000);
+    groveServo.write(-90);
+    delay(1000);
+  }
+  
+}
